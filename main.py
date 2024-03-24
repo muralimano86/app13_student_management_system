b01e27157e332ad7ac01c90c023da3e9296bf6f5
murali <muralimano86@gmail.com>
2024-03-24 20:27:07 +0000

    Student management system - Add menu bar

---
+++ main.py
@@ -0,0 +1,24 @@
+from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6.QtGui import QAction
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Student Management System")
+
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+
+        add_student_action = QAction("Add Student", self)
+        file_menu_item.addAction(add_student_action)
+
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+
+
+app = QApplication(sys.argv)
+stud_mgmt_system = MainWindow()
+stud_mgmt_system.show()
+sys.exit(app.exec())
