b8d07c1e76d3bdce1d792a371e5bce6d647a76a1
murali <muralimano86@gmail.com>
2024-03-24 20:34:00 +0000

    Student management system - Add table as central widget

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6.QtWidgets import QMainWindow, QApplication, QTableWidget
 from PyQt6.QtGui import QAction
 import sys
 
@@ -16,6 +16,11 @@ class MainWindow(QMainWindow):
 
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.setCentralWidget(self.table)
 
 
 app = QApplication(sys.argv)
